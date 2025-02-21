8db696405db6ad44ce5ccf66ab2a2bc8afee5f76
Ellie <2unmi25@naver.com>
2025-02-21 13:26:26 +0900

    feat: 활성화 상태 관리

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,5 +1,6 @@
 import { fetcher } from "../utils/fetcher.js";
-import { CHECK_EMAIL_URL, USER_JOIN_URL, USER_LOGIN_URL, USER_PAGE_URL, USER_UPDATE_URL, GET_USER_LIST_URL, USER_LOGOUT_URL } from "../utils/endpoint.js";
+import { CHECK_EMAIL_URL, USER_JOIN_URL, USER_LOGIN_URL, USER_PAGE_URL, USER_UPDATE_URL, GET_USER_LIST_URL, USER_LOGOUT_URL,
+ USER_ACTIVATE_URL, USER_DEACTIVATE_URL } from "../utils/endpoint.js";
 
 export const checkEmailDuplicate = async (email) => {
     return await fetcher(CHECK_EMAIL_URL(email));
@@ -16,21 +17,18 @@ export const login = async (loginForm) => {
     return await fetcher(USER_LOGIN_URL, {
         method: "POST",
         body: JSON.stringify(loginForm),
-        credentials: 'include',
     });
 };
 
 export const getUserData = async () => {
     return await fetcher(USER_PAGE_URL, {
         method: "GET",
-        credentials: 'include',
     });
 };
 
 export const updateUser = async (updateData) => {
     return await fetcher(USER_UPDATE_URL, {
         method: "PUT",
-        credentials: 'include',
         body: JSON.stringify(updateData),
     });
 };
@@ -38,13 +36,23 @@ export const updateUser = async (updateData) => {
 export const getUserList = async () => {
     return await fetcher(GET_USER_LIST_URL, {
         method: "GET",
-        credentials: 'include',
     });
 };
 
 export const logout = async () => {
     return await fetcher(USER_LOGOUT_URL, {
         method: "POST",
-        credentials: 'include',
+    });
+};
+
+export const activate = async (id) => {
+    return await fetcher(USER_ACTIVATE_URL(id), {
+        method: "PATCH",
+    });
+};
+
+export const deactivate = async (id) => {
+    return await fetcher(USER_DEACTIVATE_URL(id), {
+        method: "PATCH",
     });
 };
