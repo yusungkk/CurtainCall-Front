87335e7c547477085c20aafc70841a45f5144a5e
honginSon <sonhi2000@naver.com>
2025-02-24 04:09:36 +0900

    Feat: 예매 취소 추가

--- src/pages/users/OrderList.jsx
+++ src/pages/users/OrderList.jsx
@@ -1,16 +1,47 @@
 import "/src/pages/users/OrderList.css";
 import CancelBtn from "/src/components/CancelBtn.jsx";
 
-const handleOrderCancel = () => {};
+const OrderList = ({ user, orders, updateOrders }) => {
+  const fetchOrderCancel = async (orderNo) => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/orders/cancel`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ orderNo }),
+      });
 
-const OrderList = ({ user, orders }) => {
+      if (response.status === 204) {
+        alert("예매가 정상적으로 취소되었습니다.");
+        const updatedOrders = orders.filter((order) => order.orderNo !== orderNo);
+        updateOrders(updatedOrders);
+      } else {
+        throw new Error(await response.json());
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleOrderCancel = (orderNo) => {
+    if (window.confirm("정말 예매를 취소하시겠습니까?")) {
+      fetchOrderCancel(orderNo);
+    }
+  };
+
   if (!user) {
     return <div>사용자 정보를 불러올 수 없습니다.</div>;
   }
 
-  if (!orders) {
-    return <div>예매 내역이 없습니다.</div>;
+  if (orders.length === 0) {
+    return (
+      <div className="order-list-container">
+        <h3 className="empty-text">예매 내역이 없습니다.</h3>
+      </div>
+    );
   }
+
   return (
     <div className="order-list-container">
       {orders.map((order) => (
@@ -46,7 +77,10 @@ const OrderList = ({ user, orders }) => {
             <div className="order-body-right">
               <div className="order-price-text">{order.orderPrice}원</div>
               <div>
-                <CancelBtn onClick={handleOrderCancel} viewName={"예매취소"}></CancelBtn>
+                <CancelBtn
+                  onClick={() => handleOrderCancel(order.orderNo)}
+                  viewName={"예매취소"}
+                ></CancelBtn>
               </div>
             </div>
           </div>
