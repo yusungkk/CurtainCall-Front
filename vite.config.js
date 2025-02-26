eac4e81fdccac8948ca2b623289de754cb6671b6
KeeYong <ki425125@gmail.com>
2025-02-26 10:14:06 +0900

    feat: 프록시 적용 및 endPoint 수정

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
     'global': 'window',
   },
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    },
+  },
+});
